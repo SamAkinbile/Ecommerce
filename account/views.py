32a9055a24abaadda4477653b8edf82ad9704fed
Samuel Akinbile <samakinbile@gmail.com>
2024-10-03 17:02:12 +0000

    return view.py

--- account/views.py
+++ account/views.py
@@ -8,5 +8,47 @@ from django.http import HttpResponse
 
 def register(request):
 
-    return HttpResponse()
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid(): 
+
+            user = form.save()
+
+            user.is_active = False
+
+            user.save()
+
+            # Email verification setup (template)
+
+            current_site = get_current_site(request)
+
+            subject = 'Account verification email'
+
+            message = render_to_string('account/registration/email-verification.html', {
+            
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            
+            })
+
+            user.email_user(subject=subject, message=message)
+
+
+            return redirect('email-verification-sent')
+
+
+
+    context = {'form':form}
+
+
+    return render(request, 'account/registration/register.html', context=context)
+
+
+
 
